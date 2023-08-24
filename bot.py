65ede2de2598c45eac0564a2d3454d1bbe13d5bb
BroAZIK <tolliboyevazizbek21@gmail.com>
2023-08-24 13:22:36 +0500

    done

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Bot
 import time
 from pprint import pprint
 
-token = "6379988839:AAFNKWC25lG19PviPOa9jjAm1WJfPEmAJC8"
+token = "6666621362:AAFxmI1lOaRi_0Dzd0doUHvTardmX5JePpY"
 
 bot = Bot(token=token)
 
@@ -27,12 +27,42 @@ def echo():
             chat_id = message.chat.id
             text = message.text 
             photo = message.photo
+            audio = message.audio
+            document = message.document
+            video = message.video
+            location = message.location
+            contact = message.contact
+            dice = message.dice
 
-            if text != None:
+
+            if text != []:
                 bot.send_message(chat_id=chat_id, text=text)
-            elif photo != None:
-                photo_id = photo[-1].file_id
-                bot.send_photo(chat_id=chat_id, photo=photo_id)
+
+            elif audio != []:
+                file_id = audio[0].file_id
+                duration = audio[0].duration
+                bot.send_audio(chat_id=chat_id, audio=file_id, duration=duration)
+            elif photo != []: 
+                file_id = photo[0].file_id
+                bot.send_photo(chat_id=chat_id, photo=file_id)
+            elif contact != []:
+                phone_number = message.contact.phone_number
+                first_name = message.contact.first_name
+                bot.send_contact(chat_id=chat_id, phone_number=phone_number, first_name=first_name)
+            elif dice != []:
+                emoji = message.dice.emoji
+                value = message.dice.value
+                bot.send_dice(chat_id=chat_id, emoji=emoji, value=value)
+            elif animation != []:
+                file_id = message.animation.file_id
+                bot.send_animation(chat_id=chat_id, animation=file_id)
+            elif location != []:
+                latitude = message.location.latitude
+                longitude = message.location.longitude
+                bot.send_location(chat_id=chat_id, latitude=latitude, longitude=longitude)
+
+            else:
+                continue
 
             update_id = updates[-1].update_id
 
